dab364d5d70c8384f3c184a868660b6fb2b097a7
shubhamkhandelwal06 <60469125+shubhamkhandelwal06@users.noreply.github.com>
2020-01-31 15:51:29 +0530

    Add files via upload

---
+++ java-demo/MainClass.java
@@ -0,0 +1,11 @@
+package project1;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.setId(101);
+		emp.setName("shubham");
+		System.out.println(emp.getId() + emp.getName());
+	}
+
+}
---
+++ java-demo/src/test/java/Sampletest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+public class Sampletest {
+	
+	
+	@Test
+	public void simpleTest(){
+		int a= 1;
+		int b= 2;
+		assertTrue(a+b ==3);
+	}
+	
+	@Test
+	public void sampletestfall(){
+		int a= 1;
+		int b= 2;
+		assertTrue(a+b == 4); 
+	}
+
+}
